ce5e936584ee2dba2f3f67e8c9b604e8289fa86f
Tulio Bastidas <tuliobast@gmail.com>
2023-12-02 01:51:08 -0300

    modificando el archivo views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .forms import PostForm
 
