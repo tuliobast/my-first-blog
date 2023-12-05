b56776df964b507e6b4d84c2e777e95d25db6635
Tulio Bastidas <tuliobast@gmail.com>
2023-12-05 00:25:04 -0300

    creando el login y la parte de comentarios del blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+LOGIN_REDIRECT_URL = '/'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*uf-b*8pt*yvk^kqp)7g&*m^q)#m*m2@(4%kzc34@p%ycn$*cr'
 
